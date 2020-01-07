dd1b93702ff189d25f3f0fff9db34bcacd69bc57
Tina Hang <mstinahang@gmail.com>
2020-01-07 04:25:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  if (parseInt(time) < 12) {
+    return "Good Morning";
+  } else if (parseInt(time) < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
